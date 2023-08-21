3a9664262d1a14812f6a0b8ef8c7dde07370e28e
123DaNIS123 <danisnizamutdinov1@gmail.com>
2023-08-21 03:31:18 +0300

    modified setting.py again

--- refsite/settings.py
+++ refsite/settings.py
@@ -139,14 +139,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = "staticfiles/"
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = "/static"
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
-
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)

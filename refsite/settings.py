1e12d7facc4991453dbc5fca72e483c7f9779e77
123DaNIS123 <danisnizamutdinov1@gmail.com>
2023-08-21 03:11:01 +0300

    static files routes update

--- refsite/settings.py
+++ refsite/settings.py
@@ -14,8 +14,10 @@ from pathlib import Path
 
 from datetime import timedelta
 
+import os
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
 
 # Quick-start development settings - unsuitable for production
@@ -143,3 +145,7 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
